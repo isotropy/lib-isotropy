5ed885158f9c3c1b220e063d43ba70bb9e477262
Jeswin Kumar <jeswinpk@agilehead.com>
2018-02-02 12:12:33 +0530

    WIP Init

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,7 +1,15 @@
-import * as path from "path";
-import { Arguments } from "../isotropy";
+// import * as path from "path";
+// import { Arguments } from "../isotropy";
+// import http from "http"
+// var http = require('http');
+// var fs = require('fs');
 
-export async function run(args: Arguments, cwd: string) {
-  const dir =
-    typeof args.items[1] !== "undefined" ? path.resolve(args.items[1]) : cwd;
-}
+// var file = fs.createWriteStream("file.jpg");
+// var request = http.get("http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg", function(response) {
+//   response.pipe(file);
+// });
+// export async function run(args: Arguments, cwd: string) {
+//   const dir =
+//     typeof args.items[1] !== "undefined" ? path.resolve(args.items[1]) : cwd;
+  
+// }
