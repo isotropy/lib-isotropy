011b13855dd6621deb491c265e09a3064eb40aff
jeswin <jeswinpk@agilehead.com>
2018-02-03 17:45:56 +0530

    Do not init if the directory is not empty

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -5,6 +5,7 @@ import * as fse from "fs-extra";
 import download = require("download");
 import unzip = require("unzip");
 import { tmpdir } from "os";
+import exception from "../exception";
 import * as config from "../config";
 import { Arguments } from "../isotropy";
 
@@ -13,44 +14,53 @@ export function run(args: Arguments, cwd: string) {
     const dir =
       typeof args.items[1] !== "undefined" ? path.resolve(args.items[1]) : cwd;
 
-    const repo = args.named.t || `https://github.com/isotropy/hello-world`;
+    //See if the directory is non-empty
+    return !fse.existsSync(dir) || fse.readdirSync(dir).length === 0
+      ? (() => {
+          const repo =
+            args.named.t || `https://github.com/isotropy/hello-world`;
 
-    const url = repo.endsWith(".zip")
-      ? repo
-      : repo.endsWith("/")
-        ? repo + "archive/master.zip"
-        : repo + "/archive/master.zip";
+          const url = repo.endsWith(".zip")
+            ? repo
+            : repo.endsWith("/")
+              ? repo + "archive/master.zip"
+              : repo + "/archive/master.zip";
 
-    //create a temp directory.
-    const tmpDir = path.join(
-      os.tmpdir(),
-      Math.random()
-        .toString(36)
-        .substring(7)
-    );
-    download(url)
-      .pipe(unzip.Extract({ path: tmpDir }))
-      .on("close", async () => {
-        const [unzippedDir] = fse.readdirSync(tmpDir);
-        const destDir = path.join(tmpDir, unzippedDir);
-        fse.copySync(destDir, dir);
+          //create a temp directory.
+          const tmpDir = path.join(
+            os.tmpdir(),
+            Math.random()
+              .toString(36)
+              .substring(7)
+          );
+          download(url)
+            .pipe(unzip.Extract({ path: tmpDir }))
+            .on("close", async () => {
+              const [unzippedDir] = fse.readdirSync(tmpDir);
+              const destDir = path.join(tmpDir, unzippedDir);
 
-        //Read metadata
-        const projConf = await config.read(dir);
-        const jsModules = projConf.modules.filter(
-          m =>
-            m.builds &&
-            m.builds.some(b => ["typescript", "javascript"].includes(b.type))
-        );
+              fse.copySync(destDir, dir);
 
-        jsModules.forEach(m => {
-          const moduleDir = path.join(dir, m.name);
-          const result = child_process.execSync("npm install", {
-            cwd: moduleDir
-          });
-        });
+              //Read metadata
+              const projConf = await config.read(dir);
+              const jsModules = projConf.modules.filter(
+                m =>
+                  m.builds &&
+                  m.builds.some(b =>
+                    ["typescript", "javascript"].includes(b.type)
+                  )
+              );
 
-        resolve();
-      });
+              jsModules.forEach(m => {
+                const moduleDir = path.join(dir, m.name);
+                const result = child_process.execSync("npm install", {
+                  cwd: moduleDir
+                });
+              });
+
+              resolve();
+            });
+        })()
+      : exception(`The directory ${dir} is not empty.`);
   });
 }
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,7 +1,7 @@
 import "mocha";
 import "should";
 import * as path from "path";
-import * as fs from "fs";
+import * as fse from "fs-extra";
 import request = require("supertest");
 import koa from "koa";
 import isotropy from "../isotropy";
@@ -17,7 +17,29 @@ interface HttpService {
 describe("isotropy", async () => {
   it("inits", async () => {
     const projPath = path.join(cwd, "test/fixtures/hello");
-    const apps = await isotropy({ items: ["init", projPath], named: {} }, projPath);
+    const apps = await isotropy(
+      { items: ["init", projPath], named: {} },
+      projPath
+    );
+  }).timeout(25000);
+
+  it("fails to init if non-empty", async () => {
+    let error;
+    try {
+      const projPath = path.join(cwd, "test/fixtures/hello2");
+      fse.mkdirpSync(projPath);
+      fse.writeFileSync(
+        path.join(projPath, "hello.txt"),
+        "this should scare init."
+      );
+      const apps = await isotropy(
+        { items: ["init", projPath], named: {} },
+        projPath
+      );
+    } catch (ex) {
+      error = ex.message;
+    }
+    /is not empty\.$/.test(error).should.be.true();
   }).timeout(25000);
 
   it("builds", async () => {
@@ -27,9 +49,9 @@ describe("isotropy", async () => {
       cwd
     );
     ["client/dist/index.js", "server/dist/index.js"].forEach(p =>
-      fs.existsSync(path.join(projPath, p)).should.be.true()
+      fse.existsSync(path.join(projPath, p)).should.be.true()
     );
-  });
+  }).timeout(25000);
 
   it("runs", async () => {
     const projPath = path.join(cwd, "test/fixtures/basic");
@@ -49,5 +71,5 @@ describe("isotropy", async () => {
         server.close();
       })
     );
-  });
+  }).timeout(25000);
 });
